135b57177e5419eec8d666a4b03ef9c5ebfff06f
root <root@localhost.localdomain>
2018-07-23 06:41:42 +0900

    엘로퀀트 ORM

--- app/User.php
+++ app/User.php
@@ -9,24 +9,29 @@ class User extends Authenticatable
 {
 	use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-    	'name', 'email', 'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-    	'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
 
-    protected $dates = ['last_login'];
+	protected $dates = ['last_login'];
+
+	public function articles()
+	{
+		return $this->hasMany(Article::class);
+	}
 
 }
--- database/migrations/2018_07_22_211934_create_tags_table.php
+++ database/migrations/2018_07_22_211934_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticlesTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-    	Schema::create('articles', function (Blueprint $table) {
+    	Schema::create('tags', function (Blueprint $table) {
     		$table->increments('id');
-    		$table->integer('user_id')->unsigned()->index();
-    		$table->string('title');
-    		$table->text('content');
+    		$table->string('name');
+    		$table->string('slug')->index();
     		$table->timestamps();
-
-    		$table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
     	});
     }
 
@@ -31,6 +28,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-    	Schema::dropIfExists('articles');
+    	Schema::dropIfExists('tags');
     }
 }
