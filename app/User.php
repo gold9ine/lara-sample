896ca0f5a50c11e3206f20e2b929512e9d3dbd5a
root <root@localhost.localdomain>
2018-07-23 02:20:15 +0900

    user already exists error solve

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+	use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+    	'name', 'email', 'password',
     ];
 
     /**
@@ -24,6 +24,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+    	'password', 'remember_token',
     ];
+
+    protected $dates = ['last_login'];
+
+    // public function products()
+    // {
+    // 	return $this->hasMany(Product::class);
+    // }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+    	if (config('database.default') !== 'sqlite') {
+    		DB::statement('SET FOREIGN_KEY_CHECKS=0');
+    	}
+
+        // Model::unguard();
+
+    	App\User::truncate();
+    	$this->call(UsersTableSeeder::class);
+
+    	// App\Product::truncate();
+    	// $this->call(ProductsTableSeeder::class);
+
+        // Model::reguard();
+
+    	if (config('database.default') !== 'sqlite') {
+    		DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    	}
     }
 }
